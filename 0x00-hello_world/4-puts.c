8fb0c024a42e7ad425523031621638b49ab8a389
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2022-10-16 13:54:00 +0300

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - print a line of code using puts
+ *
+ * Return: O
+ */
+int main(void)
+{ 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (O);
+}
