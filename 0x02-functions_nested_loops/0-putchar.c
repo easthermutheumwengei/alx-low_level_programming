e167790329d0c8a2385cf2e47a6e30ead3a1c31d
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2022-10-18 22:29:44 +0300

    a function that prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "Esther.h"
+#include "holberton.h"
 
 /**
   * main - Entry point
@@ -7,12 +7,15 @@
   */
 int main(void)
 {
-	_putchar('E');
-	_putchar('e');
-	_putchar('t');
-	_putchar('h');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
 	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
