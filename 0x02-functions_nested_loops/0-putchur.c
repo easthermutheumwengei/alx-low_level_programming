edfe547093e70bafaf960072f37b55e18aade7b5
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2022-10-18 19:55:47 +0300

    putchar

---
+++ 0x02-functions_nested_loops/0-putchur.c
@@ -0,0 +1,19 @@
+#include "esther.h"
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	_putchar('E');
+	_putchar('s');
+	_putchar('t');
+	_putchar('h');
+	_putchar('e');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
